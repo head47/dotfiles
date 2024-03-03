57bd8bc1fe84dd813c7582248126ca1355c1e02b
head47 <63517545+head47@users.noreply.github.com>
2024-03-03 15:05:10 +0300

    reject dmenu, embrace rofi

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-doas pacman -S wireguard-tools maim xclip dmenu i3blocks \
+doas pacman -S wireguard-tools maim xclip rofi i3blocks \
     udisks2 cryptsetup sysstat jq bc wireless_tools pulseaudio \
     alsa-utils base-devel git bluez bluez-utils blueman-applet \
-    feh i3 xorg-xrdb zenity &&
+    feh i3 xorg-xrdb zenity rofi-emoji rofi-calc &&
 doas modprobe btusb &&
 doas systemctl enable --now bluetooth.service &&
 doas usermod -aG lp "$USER" &&
